f7ce64e27cdd6b6692534398b42dcaa54418c96d
Nilesh Panchotiya <109101619+NileshPatel7@users.noreply.github.com>
2025-10-05 21:57:21 +0530

    Update Eight.java

--- Eight.java
+++ Eight.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 public class Eight {
+    public static void printArray(int []arr){
+        System.out.print("{ ");
+        for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+", ");
+        }
+        System.out.print("}");
+    }
     public static int countSize(int[][] ar){
         int size=0;
         for(int i=0;i<ar.length;i++){
@@ -21,9 +28,11 @@ public class Eight {
 
         return newAr;
     }
-    public static void main(String[] args) {
-        int ar[][]={{1},{2},{5,4,12},{3},{5},{7,10}};
 
-        First.printArray(array(ar));
+
+    public static void main(String[] args) {
+        int ar[][]={{1},{2},{5,4,12},{121},{3},{5},{7,10},{5},{45}};
+
+        printArray(array(ar));
     }
 }
