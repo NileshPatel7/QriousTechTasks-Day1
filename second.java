fadb9193b6c5a936af1161ba62c09ae8d9725f9b
Nilesh Panchotiya <109101619+NileshPatel7@users.noreply.github.com>
2025-10-05 11:21:40 +0530

    Update second.java

--- second.java
+++ second.java
@@ -1,9 +1,8 @@
 public class second {
-    public static void main(String []args){
-        int []arr={1,2,3,4,5,6,7,8,9,10};
-        int k=2;
 
-        while(k!=0){
+    public static void rotateArray(int []arr,int k){
+        
+        while(k!=0 && k>0){
             int backup=arr[arr.length-1];
             for(int i=arr.length-2;i>=0;i--){
                 arr[i+1]=arr[i];
@@ -17,5 +16,10 @@ public class second {
             System.out.print(arr[i]+", ");
         }
         System.out.print("}");
+    }
+    public static void main(String []args){
+        int []arr={1,2,3,4,5,6,7,8,9,10};
+        int k=3;
+        rotateArray(arr,k);
     }
 }
