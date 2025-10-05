e98046e4e1f42187744f80c66c5b69fddb54599a
Nilesh Panchotiya <109101619+NileshPatel7@users.noreply.github.com>
2025-10-05 12:20:37 +0530

    Update Ten.java

--- Ten.java
+++ Ten.java
@@ -1,31 +1,56 @@
+class Drink{
+    String drinkName;
+    int drinkPrice;
+
+    public Drink(String dn,int dp){
+        drinkName=dn;
+        drinkPrice=dp;
+    }
+
+    public void setDrink(String dn,int dp){
+        drinkName=dn;
+        drinkPrice=dp;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "drinkName:'" + drinkName + '\'' +
+                ", drinkPrice:" + drinkPrice +
+                '}';
+    }
+}
 public class Ten {
-    public static void main(String[] args) {
-        String drinks[]={"lemonade","lime"};
-        int prices[] =   {15,10};
 
-        if(drinks.length == prices.length){
-            for(int i=0;i<drinks.length-1;i++){
-                for(int j=i+1;j<drinks.length;j++){
+    public static void printDrints(Drink[] d){
+        System.out.print("[ ");
+        for(int i=0;i<d.length;i++){
+            System.out.print(d[i]+" ");
+        }
+        System.out.print(" ]");
+    }
 
-                    if(prices[i]>prices[j]){
-                        int temp=prices[i];
-                        prices[i]=prices[j];
-                        prices[j]=temp;
-
-                        String t=drinks[i];
-                        drinks[i]=drinks[j];
-                        drinks[j]=t;
-                    }
+    public static Drink[] arrangeDrink(Drink[] d){
+        for(int i=0;i<d.length-1;i++){
+            for(int j=i+1;j<d.length;j++){
+                if(d[i].drinkPrice>d[j].drinkPrice) {
+                    Drink temp = d[i];
+                    d[i] = d[j];
+                    d[j] = temp;
                 }
             }
-            System.out.print("{ ");
-            for(int i=0;i<drinks.length;i++){
-                System.out.print("{"+"name: "+drinks[i]+" ,price: "+prices[i]+"} ,");
-            }
-            System.out.print(" }");
-        }else{
-            System.out.println("Proper data is not given{name and price}");
         }
+        return d;
+    }
+    public static void main(String[] args) {
+        Drink[] drinks={
+                        new Drink("lemonade",15),
+                        new Drink("lime",10),
+                        new Drink("Black Tea",20),
+                        new Drink("Cola",17)
+                       };
+
+        printDrints(arrangeDrink(drinks));
 
     }
 }
